e737fc07fdb73e70a4ce7fee70b15c8b483485f0
rajveer-py <85725277+rajveer-py@users.noreply.github.com>
2021-06-13 10:23:54 +0530

    Add files via upload

---
+++ age .py day3 input.py
@@ -0,0 +1,4 @@
+a=2021
+b=int(input("enter your birth year"))
+age=(a-b)
+print("you are", age,"years old")
---
+++ output age .py day 3.py
@@ -0,0 +1,15 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================== RESTART: C:/Users/tcsc/AppData/Local/Programs/Python/Python39/Day 3 age .py =======================================
+enter your birth year2005
+16
+Traceback (most recent call last):
+  File "C:/Users/tcsc/AppData/Local/Programs/Python/Python39/Day 3 age .py", line 4, in <module>
+    print("you are", age,"years old")
+NameError: name 'age' is not defined
+>>> 
+======================================== RESTART: C:/Users/tcsc/AppData/Local/Programs/Python/Python39/Day 3 age .py =======================================
+enter your birth year2005
+you are 16 years old
+>>> 
