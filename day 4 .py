64b81b5d4800d25186ee730402a7ed242cd5a477
rajveer-py <85725277+rajveer-py@users.noreply.github.com>
2021-06-15 12:12:58 +0530

    Add files via upload

---
+++ day 4 .py
@@ -0,0 +1,14 @@
+st=input("Enter a word")
+print("No y in the word is :",st.count('y'))
+
+
+#assignment 2
+
+
+str=input("Enter a string  :\n")
+print("Letters with Even Indexes are :",str[0::2])
+
+
+#assignment 3
+st=input("Enter a word again :\n")
+print("After swapcase The word is :",st.swapcase())
